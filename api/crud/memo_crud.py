f02cc763742235cf33f5f20f29a0a31016627404
tands4247 <tands4247@gmail.com>
2025-10-22 01:07:07 +0900

    GET /api/memoの実装

--- api/crud/memo_crud.py
+++ api/crud/memo_crud.py
@@ -6,19 +6,15 @@ class MemoCRUD:
     def get_memos_by_user_ids(cls, session: Session, user_ids: list[int]):
         query = """
             SELECT
-                u.id AS user_id,
-                u.name AS user_name,
-                m.id AS memo_id,
+                u.user_id,
                 m.title,
-                m.content,
-                m.created_at
+                m.summary,
+                m.content
             FROM
                 users u
-            LEFT JOIN
-                memos m ON u.id = m.user_id
+            INNER JOIN
+                memos m ON u.user_id = m.user_id
             WHERE
-                u.id = ANY(:user_ids)
-            ORDER BY
-                u.id, m.created_at DESC
+                u.user_id = ANY(:user_ids)
         """
         return RawCRUD.fetchall(session, query, {"user_ids": user_ids})
--- api/router/memo_router.py
+++ api/router/memo_router.py
@@ -1,7 +1,9 @@
 from typing import List
 from api.schema.memo_schema import MemoModel
 from api.service import memo_service
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from api.config.db_setting import get_db
+from sqlalchemy.orm import Session
 
 router = APIRouter()
 
@@ -12,7 +14,6 @@ async def create_memo():
 
 
 @router.get("/memo", response_model=List[MemoModel], tags=["memo"])
-async def get_memo():
-    
-    result = memo_service.get_memos()
+async def get_memo(session: Session = Depends(get_db)):
+    result = memo_service.get_memos(session)
     return [MemoModel(**memo) for memo in result]
